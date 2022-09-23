b08af4c03786f45e4c64f737bea4432a838b6340
Volker Hillmann <vhillmann@ad.adecc.de>
2022-09-23 23:31:26 +0200

    Ergänzung, import / export nur für Embarcadero

--- Src/Independed/BuildFileDlg.h
+++ Src/Independed/BuildFileDlg.h
@@ -1,6 +1,14 @@
 
+#if defined BUILD_WITH_VCL || defined BUILD_WITH_FMX
+
 #ifndef BUILD_FILEDLG_DLL
    #define MyFileLibAPI __declspec(dllimport)
 #else
    #define MyFileLibAPI __declspec(dllexport)
+#endif
+
+#else
+
+#define MyFileLibAPI
+
 #endif
